1b21cfaabfe9e18f9db56be2f88341aba227f238
Русанов Александр <shurik.82rusanov@yandex.ru>
2023-08-14 15:28:39 +0300

    start2

--- site_project/site_project/urls.py
+++ site_project/site_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pages/', include('pages.urls')),
+    path('posts/', include('posts.urls')),
 ]
